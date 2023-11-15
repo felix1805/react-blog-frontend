65d0deaaa3db94f17e63a86035f1c5c02f526133
felix1805 <felix1805@gmail.com>
2023-11-15 15:51:53 -0500

    structured Submission form and associated handlers.

--- src/pages/NewPostPage.js
+++ src/pages/NewPostPage.js
@@ -14,9 +14,37 @@ const NewPostPage = () => {
 
   const navigate = useNavigate()
 
+  const handleChange = e => {
+    setPost({ ...post, [e.target.name]: e.target.value })
+  }
+
   const handleSubmit = async e => {
     e.preventDefault()
     await axios.post('http://localhost:5500/posts', post)
     navigate('/')
   }
+  return (
+    <Container className="mt-4">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group>
+          <Form.Label>Title</Form.Label>
+          <Form.Control type="text" name="title" placeholder="Title" onChange={handleChange}></Form.Control>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Author</Form.Label>
+          <Form.Control type="text" name="Author" placeholder="Author" onChange={handleChange}></Form.Control>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Image URL</Form.Label>
+          <Form.Control type="text" name="image" placeholder="Image URL" onChange={handleChange}></Form.Control>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Content</Form.Label>
+          <Form.Control type="textarea" rows={5} name="content" placeholder="Content" onChange={handleChange}></Form.Control>
+        </Form.Group>
+        <Button variant="primary" type="submit">Create</Button>
+      </Form>
+    </Container>
+
+  )
 }
